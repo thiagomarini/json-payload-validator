d04c65977f68626d5c822956ac4ec87e32a972d3
Thiago Marini <thiago@weengs.co.uk>
2018-07-22 09:10:24 -0300

    Add Python 2.7 badge

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 import setuptools
 
+
 def readme():
     with open('README.rst') as f:
         return f.read()
 
+
 setuptools.setup(
     name="json_payload_validator",
-    version="0.0.2",
+    version="0.0.3",
     author="Thiago Marini",
     description="JSON validator package based on jsonschema that returns nicer validation errors for end users",
     long_description=readme(),
@@ -14,7 +16,11 @@ setuptools.setup(
     url="https://github.com/thiagomarini/json-payload-validator",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
